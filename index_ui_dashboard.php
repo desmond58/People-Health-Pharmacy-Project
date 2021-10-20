9119d314fa61347a4854c33d1ead9ac00812c6f9
Desmond <l19001577@student.newinti.edu>
2021-10-20 17:51:59 +0800

    added sample database connection use case - insert data

---
+++ index_ui_dashboard.php
@@ -0,0 +1,24 @@
+<?php
+    include_once 'includes/db_connection.php';
+?>
+
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<?php 
+    $sql_query = "SELECT * FROM products;";
+    $result = mysqli_query($conn, $sql_query);
+    $resultCheck = mysqli_num_rows($result);
+
+    //validate number of row return from database
+
+    if ($resultCheck > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo $row['product_name']."<br>";
+        }
+    }
+?>
+</body>
+</html>
